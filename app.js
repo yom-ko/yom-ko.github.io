41e3e002edfa4878416e24b275e8184f56452354
yom-ko <artem.getcode@gmail.com>
2017-12-22 19:19:20 +0300

    Add helper files and basic app layout code

---
+++ app.js
@@ -0,0 +1,11 @@
+function processSummand1(params) {
+  //
+}
+
+function processSummand2(params) {
+  //
+}
+
+function processSum(params) {
+  //
+}
---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+// Load gulp and gulp-load-plugin
+const gulp = require('gulp');
+const plugins = require('gulp-load-plugins')();
+
+// Load utility npm modules
+// const del = require('del');
+const sequence = require('run-sequence');
+const browserSync = require('browser-sync').create();
+
+
+// Task definitions start here
+
+// Development Tasks
+// -----------------
+
+// Start browserSync server
+gulp.task('browserSync', () => {
+  browserSync.init({
+    host: '192.168.1.9',
+    server: {
+      baseDir: ['./'],
+    },
+    reloadDelay: 1800,
+    notify: false,
+  });
+});
+
+// Set watchers
+gulp.task('watch', () => {
+  plugins.watch(['*.html', '*.js', '*.css'], () => {
+    browserSync.reload();
+  });
+});
+
+// Build Sequences
+// ---------------
+
+gulp.task('default', () => {
+  sequence(['browserSync'], 'watch');
+});
